ec9af8e9f80a124d64f4b829bcee4be4a93ee21a
Toni Takekawa <tonitakekawa@gmail.com>
2015-06-25 21:43:20 +0900

    first commit

---
+++ MaybeMonad/MaybeMonad.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaybeMonad
+{
+    abstract public class Option<T>
+    {
+        public abstract bool TryGetValue(out T value);
+        public static Option<T> Just(T value) { return new Just<T>(value); }
+        public static Option<T> None() { return new None<T>(); }
+
+        public abstract T Value { get; }
+
+        public bool IsNone
+        {
+            get
+            {
+                if (this is None<T>)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+
+    class Just<T> : Option<T>
+    {
+        private readonly T _value;
+        public Just(T value) { _value = value; }
+        public override bool TryGetValue(out T value)
+        {
+            value = _value;
+            return true;
+        }
+
+        public override T Value { get { return _value; } }
+    }
+
+    class None<T> : Option<T>
+    {
+        public None() { }
+        public override bool TryGetValue(out T value)
+        {
+            value = default(T);
+            return false;
+        }
+
+        public override T Value { get { return default(T); } }
+    }
+
+    static class Maybe
+    {
+        public static Option<B> Select<A, B>(this Option<A> option, Func<A, B> selector)
+        {
+            A value;
+            return option.TryGetValue(out value) ?
+                Option<B>.Just(selector(value)) :
+                Option<B>.None();
+        }
+
+        public static Option<B> SelectMany<A, B>(this Option<A> option, Func<A, Option<B>> selector)
+        {
+            A value;
+            return option.TryGetValue(out value) ?
+                selector(value) :
+                Option<B>.None();
+        }
+
+        public static Option<C> SelectMany<A, B, C>(this Option<A> option, Func<A, Option<B>> selector, Func<A, B, C> projector)
+        {
+            A val0;
+            B val1;
+            return (option.TryGetValue(out val0) && selector(val0).TryGetValue(out val1)) ?
+                Option<C>.Just(projector(val0, val1)) :
+                Option<C>.None();
+        }
+    }
+}
---
+++ MaybeMonad/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaybeMonad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+        }
+
+        Program()
+        {
+            var a = from x in "1".ParseInt()
+                    from y in "2".ParseInt()
+                    from z in "3".ParseInt()
+                    // コメントアウトするとa.IsNoneがtrueになる
+                    // from e in "あ".ParseInt()
+                    select x + y + z;
+
+            if (a.IsNone)
+            {
+                Console.WriteLine("失敗");
+                return;
+            }
+
+            Console.WriteLine(a.Value);
+        }
+    }
+
+    static class StringEx
+    {
+        public static Option<int> ParseInt(this string s)
+        {
+            int v;
+            if (int.TryParse(s, out v))
+            {
+                return Option<int>.Just(v);
+            }
+
+            return Option<int>.None();
+        }
+    }
+}
+
